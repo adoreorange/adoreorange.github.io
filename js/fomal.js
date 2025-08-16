306fb4bfe3fe68989b32956ce8ad83b93a9bf38e
adore_org <1393080342@qq.com>
2025-08-16 19:41:40 +0800

    Site updated: 2025-08-16 19:41:40

--- js/fomal.js
+++ js/fomal.js
@@ -1281,8 +1281,8 @@ function share_() {
   try {
     // 截取标题
     var title = document.title;
-    var subTitle = title.endsWith("| Fomalhaut🥝") ? title.substring(0, title.length - 14) : title;
-    navigator.clipboard.writeText('Fomalhaut🥝的站内分享\n标题：' + subTitle + '\n链接：' + url + '\n欢迎来访！🍭🍭🍭');
+    var subTitle = title.endsWith("| adore🍊") ? title.substring(0, title.length - 14) : title;
+    navigator.clipboard.writeText('adore🍊的站内分享\n标题：' + subTitle + '\n链接：' + url + '\n欢迎来访！🍭🍭🍭');
     new Vue({
       data: function () {
         this.$notify({
